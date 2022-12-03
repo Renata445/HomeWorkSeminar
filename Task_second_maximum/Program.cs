6af31609c41322cd81910d784c49d238fca419dc
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-03 20:28:41 +0300

    Task_second_maximum

---
+++ Task_second_maximum/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int max1 = 0;
+int max2 = 0;
+int n = Convert.ToInt32(Console.ReadLine());
+while(n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if(n > max1)
+    {
+        max2 = max1;
+        max1 = n;
+    }
+    else if(n > max2)
+    {
+        max2 = n;
+    }
+}
+Console.WriteLine($"Второе максимальное значение = {max2}");
