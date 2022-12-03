26575ca2f467426002830560fafa0eb4a7de91c4
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-03 19:47:53 +0300

    Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n3 = 0;
+if(n > 100)
+{
+    while(n > 999)
+    {
+        n = n / 10;
+    }
+    n3 = n % 10;
+    Console.WriteLine(n3);
+}
+
+if(n <= 99)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
