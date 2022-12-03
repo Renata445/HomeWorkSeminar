e664eb0617cd88654c5189376fbe01e8ddbafa5b
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-03 19:21:07 +0300

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите 3-ех значное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n2 = (n % 100)/10;
+Console.WriteLine($"Второе число = {n2}");
