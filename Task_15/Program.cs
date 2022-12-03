dd19589ea837dba2ecb69eb4be233af37c046b79
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-03 19:56:24 +0300

    Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n == 6 || n == 7)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
