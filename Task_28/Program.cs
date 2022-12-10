a11e4d68dad52dc9f99d7dceb8d6a48150f1821b
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-10 15:27:38 +0300

    Task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int proiz = 1;
+for(int i = 2; i <= n; i++)
+{
+    proiz *= i;
+}
+Console.WriteLine(proiz);
